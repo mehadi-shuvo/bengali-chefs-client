f3f1d064f56af8cd35c819443d275e8bda1e3715
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-02 08:04:17 +0600

    header and footer added

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from '../pages/shared/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/shared/Footer/Footer';
 
 const MainLayouts = () => {
     return (
         <div>
-            layout
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
