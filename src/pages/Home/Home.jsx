217439d6b5b652d024c3a7ff0e7dcd447854b145
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-02 08:53:51 +0600

    banner section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import banner from '../../assets/banner.jpg';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            home
+        <div className='banner pb-12 pt-48'>
+            <div className='w-4/5 mx-auto text-center'>
+                <h4 className=' text-6xl font-bold text-white'>World class Bengali <br></br><span className='text-red-500'>Chefs and Foods</span><br></br> are here</h4>
+                
+                    <p className=' text-base font-light text-white md:px-10 mt-4'>Sed ut perspiciatis unde omnis iste natus totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    <button className='py-2 px-4 bg-red-500 text-white mt-7 font-semibold'>Learn More</button>
+
+            </div>
         </div>
     );
 };
--- src/routes/MainRoute.jsx
+++ src/routes/MainRoute.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayouts></MainLayouts>,
+      children:[
+        {
+          path:'/',
+          element: <Home></Home>
+        }
+      ]
     },
   ]);
 
