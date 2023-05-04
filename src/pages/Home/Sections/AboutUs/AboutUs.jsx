0a32adaa3c97c6c23bf15e2c35387c8be3f5be22
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-04 09:10:55 +0600

    comments section done and about section update

--- src/pages/Home/Sections/AboutUs/AboutUs.jsx
+++ src/pages/Home/Sections/AboutUs/AboutUs.jsx
@@ -4,19 +4,15 @@ import './AboutUs.css'
 
 const AboutUs = () => {
     return (
-        <div className='w-4/5 mx-auto md:grid grid-cols-2 mt-14 gap-5'>
-                        <div>
-                <h4 className='text-4xl font-extrabold text-red-500'>About Us</h4>
-                <p className='text-lg font-light text-slate-700 mt-3'>We provide Bangladesh's No.1 food and our chefs are too much qualified</p>
-                <p className='text-lg font-light text-slate-800 mt-5'>Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit amet magna suscipit, fermentum mattis erat rutrum. Sed suscipit libero lectus, at ullamcorper erat feugiat eu. Nam posuere ultrices nibh ut sagittis. Etiam arcu turpis, elementum ac nulla vel, tristique cursus libero. Fusce feugiat, justo at mattis tincidunt, velit ante congue ante, et lacinia metus ipsum a risus.
-
-                    Vivamus hendrerit porta libero nec imperdiet. Praesent interdum nisl nec facilisis dapibus</p>
-                    <button className='py-2 px-4 bg-red-500 text-white mt-7 font-semibold'>Learn More</button>
+        <div className="hero bg-base-200">
+            <div className="hero-content w-4/5 mx-auto flex-col lg:flex-row">
+                <img src={aboutUs} className="max-w-sm mask mask-hexagon shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">About BengaliChefs</h1>
+                    <p className="py-6">BengaliChefs prepares and serves exquisite dinners to parties from two to 200 at your home, business or private catering event. His blend of French-Mediterranean cuisine draws on a lifetime of skills and the knowledge that all meals, no matter how large or how small, are special events among family and friends</p>
+                    <button className="btn py-2 px-4 bg-red-500 text-white font-semibold border-none">Get Started</button>
+                </div>
             </div>
-            <div className='about-us w-full '>
-                <img className=' w-full' src={aboutUs} alt="" />
-            </div>
-
         </div>
     );
 };
--- src/pages/Home/Sections/Comments/Comments.jsx
+++ src/pages/Home/Sections/Comments/Comments.jsx
@@ -1,10 +1,138 @@
 import React from 'react';
+import pic1 from '../../../../assets/p1.jpg'
+import pic2 from '../../../../assets/p2.jpg'
+import pic3 from '../../../../assets/p3.jpg'
+import pic4 from '../../../../assets/p4.jpg'
 
 
 const Comments = () => {
     return (
-        <div>
-            
+        <div className='mt-14'>
+            <h1 className='text-center text-4xl text-red-500 font-bold mb-11'>Our Clint Comments</h1>
+            <div className="carousel w-4/5 mx-auto">
+                <div id="item1" className="carousel-item w-full">
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic1} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Tamim Tamu</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic2} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Humaira Akter</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic3} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Rafi Rock</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="item2" className="carousel-item w-full">
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic4} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Chirli Chok</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic3} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Rafi Rock</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic2} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Humaira Akter</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="item3" className="carousel-item w-full">
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic3} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Rafi Rock</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic1} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Tamim Tamu</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic2} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Humaira Akter</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="item4" className="carousel-item w-full">
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic4} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Chirli Chok</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic2} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Humaira Akter</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                    <div className="card bg-slate-400 mx-2">
+                        <figure className="px-10 pt-10">
+                            <img src={pic3} alt="Shoes" className="mask mask-squircle h-24" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Rafi Rock</h2>
+                            <p>Thank you for the amazing meal! Looking forward to visiting again soon!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center w-full py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
+                <a href="#item3" className="btn btn-xs">3</a>
+                <a href="#item4" className="btn btn-xs">4</a>
+            </div>
         </div>
     );
 };
